6ec1835fa37b7273f1f5e7385402eef64a67ef91
Tom Hetmer <tom@hetmer.com>
2023-07-27 02:45:45 -0300

    synced on 2023-07-27 02:45:45

--- roles/common/files/config.fish
+++ roles/common/files/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     set -U fish_greeting
     set -Ux EDITOR nano
 
-    alias wget='curl -O'
+    alias wget='curl -O -L'
     #alias nano='micro'
     alias l='ls -lAh'
 end
