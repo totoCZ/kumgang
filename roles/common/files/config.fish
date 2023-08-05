92eb9dd5d932d2bbe809a4a86b4de1ec8e62fa6f
Tom Hetmer <tom@hetmer.com>
2023-08-04 21:41:14 -0400

    synced on 2023-08-04 21:41:14

--- roles/common/files/config.fish
+++ roles/common/files/config.fish
@@ -1,8 +1,16 @@
 if status is-interactive
     set -U fish_greeting
-    set -Ux EDITOR nano
+    set -Ux EDITOR micro
+    set -Ux PAGER less
 
     alias wget='curl -O -L'
-    #alias nano='micro'
+    alias nano='micro'
     alias l='ls -lAh'
+
+    function last_history_item
+        echo $history[1]
+    end
+    abbr -a !! --position anywhere --function last_history_item
+
+    printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "fish"}}\x9c'
 end
